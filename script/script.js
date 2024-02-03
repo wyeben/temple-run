bdffd017ec23a2c735ff4e4463213995722f8e19
Wyeben <yila.ben@yahoo.com>
2024-02-03 08:10:28 +0100

    added code

--- script/script.js
+++ script/script.js
@@ -53,7 +53,7 @@ function update() {
     if (
       playerX < obstacle.x + obstacle.width &&
       playerX + 30 > obstacle.x &&
-    
+  
       playerY < obstacle.y + obstacle.height &&
       playerY + 30 > obstacle.y
     ) {
