b0ea21e6ca4153645a9bb608d1ef79d0d1ecb509
Wyeben <yila.ben@yahoo.com>
2024-05-15 16:22:27 +0100

    Added changes

---
+++ script/test.js
@@ -0,0 +1,21 @@
+// let time = 13;
+
+// if (time == 12){
+//     console.log('Good afternoon')
+// }else if (time < 12){
+//     console.log('Good morning')
+// }else{
+//     console.log('Good evening')
+// }
+
+// function greet(n){
+//     console.log('Hello'+ n )
+// }
+
+// greet(' Ben')
+
+function add(n,r){
+    console.log(n+r)
+}
+
+add(5,7)
