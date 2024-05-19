4040ed953ddbbb2a6085ae3ed8e767974abf1cd5
Wyeben <yila.ben@yahoo.com>
2024-05-19 18:03:20 +0100

    Added changes

--- script/test.js
+++ script/test.js
@@ -1,5 +1,15 @@
-for(let i = 1 ; i <= 7; i ++){
-    console.log('*')
+let grid = 8;
+let chessBoard = '';
 
-    
+for (let row = 0; row < grid; row++){
+    let line = '';
+    for(let col = 0; col < grid; col++){
+        if ((row + col) % 2 == 0){
+            line += ' ';
+        }else{
+            line += '#';
+        }
+    }
+    chessBoard += line + '\n';
 }
+console.log(chessBoard)
