246cc02502b7c6eaffc04c0d33a482469ad390d3
Wyeben <yila.ben@yahoo.com>
2024-05-21 07:49:43 +0100

    Added changes

---
+++ script/test2.js
@@ -0,0 +1,78 @@
+// let isStudent =  false;
+
+// let fee = isStudent ? 200 : 600;
+
+// console.log(fee)
+
+// let week = 'Tuesday';
+
+// switch (week){
+//     case 'Monday':
+//         console.log('First day of the week');
+//         break
+//     case 'Friday':
+//         console.log('Last day of the week');
+//         break
+//     default:
+//         console.log('Mid')
+
+
+// }
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for loop
+
+// for(let number = 1; number <= 10; number++){
+//     console.log(number)
+// }
+
+// while loop
+
+// let number = 1;
+
+// while(number <= 10){
+//     console.log(number)
+//     number++
+// }
+
+// Do while loop
+
+// let number = 1;
+
+// do {
+//     console.log(number)
+//     number++
+// }while(number <= 10)
+
+// for... of loop
+
+// let numbers = [1,2,3,4,5];
+
+// for(number of numbers){
+//     console.log(number)
+// }
+
+// write a loop that count 1 to 1000
+
+// for(let i = 1; i <= 10; i++){
+//     if(i % 2 == 1)
+//     console.log(i)
+// }
+
+// for ... in loop
+
+let person = {name: 'Benson', age: 90}
+
+for(let key in person){
+    console.log(key + ':' + person[key])
+}
